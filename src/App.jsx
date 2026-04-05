8b9c30349200c657d297d7a9d08889225c2943ca
Manukagallage7 <manukakavinda1110@gmail.com>
2026-04-05 15:48:30 +0530

    feat: implement main application structure with routing

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/homePage.jsx"
+import LoginPage from "./pages/loginPage.jsx"
+import RegisterPage from "./pages/registerPage.jsx"
+import AdminPage from "./pages/adminPage.jsx"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
