98371a5264045e90a61899f5d368f67455f1fbe9
Manukagallage7 <manukakavinda1110@gmail.com>
2026-04-07 15:41:52 +0530

    feat: add Toaster component for notifications in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import HomePage from "./pages/homePage.jsx"
 import LoginPage from "./pages/loginPage.jsx"
 import RegisterPage from "./pages/registerPage.jsx"
 import AdminPage from "./pages/adminPage.jsx"
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Toaster position="top-right"/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
