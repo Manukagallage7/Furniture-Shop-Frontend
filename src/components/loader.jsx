0d67b3b5d2ea84b88c15d7b5cac24877c87f3da6
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-05 23:54:41 +0530

    feat: add Loader component for loading indication

---
+++ src/components/loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className='w-full h-64 flex items-center justify-center'>
+            <span className='animate-spin text-4xl text-amber-600'>⏳</span>
+        </div>
+    );
+}
