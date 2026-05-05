19cc2806ce475b55211cab2e67bcd5a3dc51b881
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-05 23:54:27 +0530

    feat: remove HomePage route and update routing to include ClientPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from "./pages/homePage.jsx"
 import LoginPage from "./pages/loginPage.jsx"
 import RegisterPage from "./pages/registerPage.jsx"
 import AdminPage from "./pages/adminPage.jsx"
 import { Toaster } from "react-hot-toast";
+import ClientPage from "./pages/client/clientPage.jsx"
 
 function App() {
   return (
@@ -11,10 +11,10 @@ function App() {
       <div>
         <Toaster position="top-right"/>
         <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/*" element={<ClientPage />} />
         </Routes>
       </div>
     </BrowserRouter>
