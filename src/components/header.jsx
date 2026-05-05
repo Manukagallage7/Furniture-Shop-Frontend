9047b9e28a635ea0187b1cf29fa3473d9bd340f0
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-05 23:54:34 +0530

    feat: add Header component with navigation links

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className="bg-blue-500 flex justify-center items-center gap-8">
+            <Link to="/" className="text-white text-2xl font-bold py-4">Home</Link>
+            <Link to="/products" className="text-white text-2xl font-bold py-4">Products</Link>
+            <Link to="/about-us" className="text-white text-2xl font-bold py-4">About Us</Link>
+            <Link to="/contact-us" className="text-white text-2xl font-bold py-4">Contact Us</Link>
+        </header>
+    )
+}
+    
