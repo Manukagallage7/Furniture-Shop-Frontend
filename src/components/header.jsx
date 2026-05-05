2bfac2b777ed7216979bbff14dcfa79b806eb9b1
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-06 02:30:18 +0530

    feat: add Cart link to Header component for improved navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
         <header className="bg-blue-500 flex justify-center items-center gap-8">
             <Link to="/" className="text-white text-2xl font-bold py-4">Home</Link>
             <Link to="/products" className="text-white text-2xl font-bold py-4">Products</Link>
+            <Link to="/cart" className="text-white text-2xl font-bold py-4">Cart</Link>
             <Link to="/about-us" className="text-white text-2xl font-bold py-4">About Us</Link>
             <Link to="/contact-us" className="text-white text-2xl font-bold py-4">Contact Us</Link>
         </header>
