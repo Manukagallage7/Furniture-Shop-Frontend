c66ed259988805bc1ba2fb0ec5d494c1126e2a79
Manukagallage7 <manukakavinda1110@gmail.com>
2026-05-10 11:44:31 +0530

    feat: add "My Orders" link to the header for easy navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
             <Link to="/products" className="text-white text-2xl font-bold py-4">Products</Link>
             <Link to="/about-us" className="text-white text-2xl font-bold py-4">About Us</Link>
             <Link to="/contact-us" className="text-white text-2xl font-bold py-4">Contact Us</Link>
+            <Link to="/my-orders" className="text-white text-2xl font-bold py-4">My Orders</Link>
             <Link to="/cart" className=" absolute right-30 text-white text-2xl font-bold py-4">
                 <BiCart className="text-white text-3xl ml-4" />
             </Link>
